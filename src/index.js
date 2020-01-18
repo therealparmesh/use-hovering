8730366a195748c89b5f947f4df9c251d0faf3f5
Parmesh Krishen <therealparmesh@outlook.com>
2020-01-18 17:55:38 -0600

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const useHovering = () => {
+  const [hovering, setHovering] = React.useState(false);
+
+  const bind = React.useMemo(
+    {
+      onMouseEnter: () => setHovering(true),
+      onMouseLeave: () => setHovering(false),
+      onFocus: () => setHovering(true),
+      onBlur: () => setHovering(false),
+      tabIndex: 0,
+    },
+    [],
+  );
+
+  return [hovering, bind];
+};
